5bd9282275ca9c7915ffb8686e2b1f93d8cd043b
NedLandy <matheus.v4200@gmail.com>
2020-10-03 19:46:39 -0300

    display manager

--- src/components/display/display.js
+++ src/components/display/display.js
@@ -1,7 +1,20 @@
 const component = require('../component');
+const PackageDisplay = require('./displays/packageDisplay');
 
 module.exports =  class Display extends component{
 	constructor(){
 		super();
+		this.htmlComponent = document.createElement('div');
+		this.packageDisplay = new PackageDisplay();
+		this.displayGroup = {};
+	}
+	setCurrentDisplay(newOptionGroupName){
+		this.htmlComponent.innerHTML = '';
+		this.htmlComponent.appendChild(this.displayGroup[newOptionGroupName])
+	}
+	build(){
+		document.getElementById('display').appendChild(this.htmlComponent);
+		this.displayGroup['PackageDisplay'] = this.packageDisplay.htmlComponent;
+		this.setCurrentDisplay('PackageDisplay');
 	}
 }
