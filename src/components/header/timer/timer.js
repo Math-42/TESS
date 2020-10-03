d264459b2de0060d6e0c1be8f8174b17f7674ddf
NedLandy <matheus.v4200@gmail.com>
2020-10-03 18:32:53 -0300

    adicionando timer terra/marte

--- src/components/header/timer/timer.js
+++ src/components/header/timer/timer.js
@@ -6,30 +6,57 @@ module.exports = class Header extends component {
 		this.htmlComponent = document.createElement('div');
 		this.earthClock = document.createElement('div');
 		this.marsClock = document.createElement('div');
+		this.startedAt;
 		this.build();
 	}
-	changeTime() {
-		let today = new Date();
-		let h = today.getHours();
-		let m = today.getMinutes();
-		let s = today.getSeconds();
-		m = (m < 10) ? 0 + m : m;
-		s = (s < 10) ? 0 + s : s;
-		this.marsClock.textContent = h + ":" + m + ":" + s;
+	changeEarthTime() {
+		let horario = new Date();
+		let h = horario.getHours();
+		let m = horario.getMinutes();
+		let s = horario.getSeconds();
+		h = (parseInt(m) < 10) ? '0' + h : h;
+		m = (parseInt(m) < 10) ? '0' + m : m;
+		s = (parseInt(s) < 10) ? '0' + s : s;
 		this.earthClock.textContent = h + ":" + m + ":" + s;
-		this.startTime()
+	}
+	changeMarsTime() {
+		let horario = new Date();
+		let h = horario.getHours();
+		let m = horario.getMinutes();
+		let s = horario.getSeconds();
+		h = (parseInt(m) < 10) ? '0' + h : h;
+		m = (parseInt(m) < 10) ? '0' + m : m;
+		s = (parseInt(s) < 10) ? '0' + s : s;
+		this.marsClock.textContent = h + ":" + m + ":" + s;
+
 	}
 	startTime() {
-		setTimeout(()=>{window.dispatchEvent(new CustomEvent('setTime'))}, 500);
+		setTimeout(() => {
+			window.dispatchEvent(new CustomEvent('setEarthTime'))
+		}, 1);
+		setTimeout(() => {
+			window.dispatchEvent(new CustomEvent('setMarsTime'))
+		}, 1);
 	}
 	build() {
+		this.startedAt = new Date();
 		this.htmlComponent.classList.add('glow');
 		this.htmlComponent.appendChild(this.marsClock)
 		this.htmlComponent.appendChild(this.earthClock)
 
-		window.addEventListener('setTime', () => {
-			this.changeTime();
-
+		window.addEventListener('setEarthTime', () => {
+			this.changeEarthTime();
+			setTimeout(() => {
+				window.dispatchEvent(new CustomEvent('setEarthTime'))
+			}, 500);
 		});
+
+		window.addEventListener('setMarsTime', () => {
+			this.changeMarsTime();
+			setTimeout(() => {
+				window.dispatchEvent(new CustomEvent('setMarsTime'))
+			}, 1433);
+		});
+
 	}
 }
