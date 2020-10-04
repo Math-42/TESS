f9c7b10b875ad60f85bdcbc4a10e37857287deb6
NedLandy <matheus.v4200@gmail.com>
2020-10-04 11:54:04 -0300

    alterando menu do DSN

--- src/components/display/displays/DSNdisplay/DSNdisplay.js
+++ src/components/display/displays/DSNdisplay/DSNdisplay.js
@@ -1,97 +1,34 @@
 const StandardDisplay = require('../../standardDisplay');
+const Antenna = require('./antenna');
 
 module.exports =  class Display extends StandardDisplay{
 	constructor(){
 		super('DSNdisplay');
-		//this.build();
-    }
-    createButtons(){
-        let i = 0;
-        let spacecraftList = ["InSight Lander", "Maren", "Mars Odyssey", "Mars 2020", "Mars Reconnaissance Orbiter"]
 
-        this.nextArrow = document.createElement('button')
-        this.nextArrow.classList = 'col btn btn-default btn-ghost glow option'
-        this.nextArrow.textContent = '>'
+        this.htmlComponent = document.createElement('fieldset');
+        this.htmlComponent.classList.add('package');
 
-        this.backArrow = document.createElement('button')
-        this.backArrow.classList = 'col btn btn-default btn-ghost glow option'
-        this.backArrow.textContent = '<'
+        this.title = document.createElement('legend');
+        this.title.classList.add('packageTitle');
+        this.title.textContent = 'Deep Space Network'
 
-        this.option = document.createElement('button')
-        this.option.classList = 'col btn btn-default btn-ghost glow option DSNoption'
-        this.option.textContent = spacecraftList[i]
+        this.htmlComponent.appendChild(this.title)
 
-        document.getElementById('DSNrow').appendChild(this.backArrow)
-        document.getElementById('DSNrow').appendChild(this.option)
-        document.getElementById('DSNrow').appendChild(this.nextArrow)
-        
-        this.nextArrow.addEventListener("click", () => {
-            if (i >= 4){
-                i = 0;
-            } else {
-                i++;
-            }
-            this.option.textContent = spacecraftList[i];
-        });
+        this.htmlContainer = document.createElement('div');
+        this.htmlContainer.style.display = 'flex';
 
-        this.backArrow.addEventListener("click", () => {
-            if (i <= 0){
-                i = 4;
-            } else {
-                i--;
-            }
-            this.option.textContent = spacecraftList[i];
-        }
-        );
-    }
-    activatedSatellite(){
-        this.activeSatellite = document.createElement('img')
-        this.activeSatellite.draggable = false;
-        this.activeSatellite.classList.add('col-3')
-        this.activeSatellite.src = "../assets/images/Satellite.gif"
-        this.activeSatellite.classList.add("Satellite")
-        document.getElementById('satelliteRow').appendChild(this.activeSatellite)
-    }
-    deactivatedSatellite(){
-        this.deactiveSatellite = document.createElement('img')
-        this.deactiveSatellite.draggable = false;
-        this.deactiveSatellite.classList.add('col-3')
-        this.deactiveSatellite.src = "../assets/images/Satellite.png"
-        this.deactiveSatellite.classList.add("Satellite")
-        document.getElementById('satelliteRow').appendChild(this.deactiveSatellite)
+        this.htmlComponent.appendChild(this.htmlContainer);
+
+        this.Antenna1 = new Antenna('Spain',3);
+        this.Antenna2 = new Antenna('Spain',1);
+        this.Antenna3 = new Antenna('USA',1);
+        this.Antenna4 = new Antenna('Australia',1);
     }
 	build(){
-		this.htmlComponent.innerHTML = `
-			<fieldset class="package">
-				<legend class="packageTitle">Deep Space Network</legend>
-                <div class= "row" id = "satelliteRow">              
-                    
-                </div>
-                <div class= "row">              
-                    <img src="../assets/images/Spain.png" class="flags col-3" draggable="false">            
-                    <img src="../assets/images/Spain.png" class="flags col-3" draggable="false">  
-                    <img src="../assets/images/USA.png" class="flags col-3" draggable="false">  
-                    <img src="../assets/images/Australia.png" class="flags col-3" draggable="false">  
-                </div>
-                <div class= "row" id="DSNrow">              
-                    
-                </div>
-                <div class= "row" id="DSNrow"> 
-                    
-                </div>
-                <div class= "row" id="DSNrow">
-                    
-                </div>
-			</fieldset>
-        `
-        this.activatedSatellite();
-        this.activatedSatellite();
-        this.activatedSatellite();
-        this.deactivatedSatellite();
-
-        this.createButtons();
-        this.createButtons();
-        this.createButtons();
-        this.createButtons();
+        this.htmlContainer.classList.add('row');
+        this.htmlContainer.append(this.Antenna1.htmlComponent);
+        this.htmlContainer.append(this.Antenna2.htmlComponent);
+        this.htmlContainer.append(this.Antenna3.htmlComponent);
+        this.htmlContainer.append(this.Antenna4.htmlComponent);
 	}
 }
