5db77159ccfb55844b4ace839b51e6eb82a2629f
NedLandy <matheus.v4200@gmail.com>
2020-10-04 05:32:07 -0300

    alterando caminho para as imagens

--- src/components/display/displays/DSNdisplay/DSNdisplay.js
+++ src/components/display/displays/DSNdisplay/DSNdisplay.js
@@ -29,7 +29,7 @@ module.exports =  class Display extends StandardDisplay{
         this.activeSatellite = document.createElement('img')
         this.activeSatellite.draggable = false;
         this.activeSatellite.classList.add('col-3')
-        this.activeSatellite.src = "../images/Satellite.gif"
+        this.activeSatellite.src = "../assets/images/Satellite.gif"
         this.activeSatellite.classList.add("Satellite")
         document.getElementById('satelliteRow').appendChild(this.activeSatellite)
     }
@@ -37,7 +37,7 @@ module.exports =  class Display extends StandardDisplay{
         this.deactiveSatellite = document.createElement('img')
         this.deactiveSatellite.draggable = false;
         this.deactiveSatellite.classList.add('col-3')
-        this.deactiveSatellite.src = "../images/Satellite.png"
+        this.deactiveSatellite.src = "../assets/images/Satellite.png"
         this.deactiveSatellite.classList.add("Satellite")
         document.getElementById('satelliteRow').appendChild(this.deactiveSatellite)
     }
@@ -49,10 +49,10 @@ module.exports =  class Display extends StandardDisplay{
                     
                 </div>
                 <div class= "row">              
-                    <img src="../images/Spain.png" class="flags col-3" draggable="false">            
-                    <img src="../images/Spain.png" class="flags col-3" draggable="false">  
-                    <img src="../images/USA.png" class="flags col-3" draggable="false">  
-                    <img src="../images/Australia.png" class="flags col-3" draggable="false">  
+                    <img src="../assets/images/Spain.png" class="flags col-3" draggable="false">            
+                    <img src="../assets/images/Spain.png" class="flags col-3" draggable="false">  
+                    <img src="../assets/images/USA.png" class="flags col-3" draggable="false">  
+                    <img src="../assets/images/Australia.png" class="flags col-3" draggable="false">  
                 </div>
                 <div class= "row" id="DSNrow">              
                     
