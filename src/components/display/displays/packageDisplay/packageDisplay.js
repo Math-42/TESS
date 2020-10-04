71b0471739c46191c5fb483c4d75817b75d1f1e0
NedLandy <matheus.v4200@gmail.com>
2020-10-04 03:57:41 -0300

    abrir pacote no mesmo menu

--- src/components/display/displays/packageDisplay/packageDisplay.js
+++ src/components/display/displays/packageDisplay/packageDisplay.js
@@ -5,16 +5,35 @@ const fs = require('fs');
 module.exports = class PackageDisplay extends StandardDisplay {
 	constructor() {
 		super('PackageDisplay');
+		this.packages = {};
 	}
+
 	addPackage(packagePath) {
+		const myNotification = new Notification('ZenJogo', {
+			body: 'New package arrived!!'
+		})
+
 		let newPackage = new Package(packagePath);
-		this.htmlComponent.appendChild(newPackage.htmlComponent);
+		this.packages[newPackage.title] = newPackage.htmlComponent;
 	}
+
+	openPackage() {
+		this.htmlComponent.innerHTML = '';
+	}
+
 	build() {
 		this.addPackage('./src/packages/mission1.json');
 
 		window.addEventListener('addNewPackage', (evt) => {
 			this.addPackage(evt.detail);
+		});
+
+		window.addEventListener('openPackage', (evt) => {
+			window.dispatchEvent(new CustomEvent('showDisplay', {
+				detail: 'PackageDisplay',
+			}));
+			console.log(evt.detail,this.packages);
+			this.htmlComponent.appendChild(this.packages[evt.detail]);
 		});
 	}
 }
